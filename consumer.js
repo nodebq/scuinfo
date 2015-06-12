ec9ce7aa0cf3199e7bb6a70506071feb464f9243
dsgygb <dsgygb@126.com>
2015-06-13 04:30:52 +0800

    xxx

--- consumer.js
+++ consumer.js
@@ -1,5 +1,6 @@
 var conn= require('./libs/mysql.js');
 var common= require('./libs/common.js');
+var request = require('request');
 /**
  * 消费者
  * @type {{}}
@@ -8,6 +9,25 @@ var consumer={
 
 };
 
+var weiboToken = {
+
+};
+
+fs.readFile('./token/weibo_token.txt', 'utf8', function (err, txt) {
+    //console.log(err,txt);//return;
+    if (err) {
+        console.log('微博初始化失败');
+        return;
+    }
+
+    try{
+        weiboToken=JSON.parse(txt);
+        console.log('初始化微博分享');
+    }catch(e){
+        weiboToken={}
+    }
+    
+});
 /**
  * 微博发布消费者
  */
@@ -26,6 +46,73 @@ consumer.weibo = function(){
 
 
                 //todo 发布到微博,改状态
+
+                conn.query(
+
+                    {
+                        sql:"select * from secret_post where id="+r[0].postId
+                    },function(ee,rr){
+
+                        if(ee){
+                            console.log(ee);
+                            return;
+                        }
+                        
+                        if(rr.length>0){
+
+                            request.post(
+                                {
+                                    url:"https://api.weibo.com/2/statuses/update.json",
+                                    form:{
+                                        status:encodeURIComponent(rr[0].content.substr(0,138)),
+                                        annotations:JSON.stringify({
+                                            secret: rr[0].secret,
+                                            userId:rr[0].userId
+                                        })
+                                    }
+                                },function(eee,rrr,bbb){
+
+                                    try{
+                                        var userInfo = JSON.parse(rrr.body);
+                                    }catch(e){
+                                        var userInfo = {
+                                            error_code:20000
+                                        }
+                                    }
+                                    //console.log(userInfo);
+
+                                    //todo 判断是否能拿到用户资料，如果能拿到的话，正常执行登录/注册流程
+                                    // 如果拿不到的话，跳到redirect页，并设置 session.userStatus:wechatNotFans
+                                    //
+
+                                    if(userInfo.error_code){
+                                        console.log(userInfo);
+                                        return;
+                                    }
+
+                                    //改状态
+                                    conn.query(
+                                        {
+                                            sql:"update secret_weibo_query set status=1,postAt="+common.time()+",weiboId="+userInfo.id
+                                        },function(eeeee,rrrrr){
+                                            console.log(eeeee,'成功发布一条微博');
+                                        }
+                                    )
+
+
+                    }
+                            )
+
+                            
+                        }else{
+                            console.log('该帖子已被删除');
+                        }
+
+                    }
+                )
+
+
+
 
 
 

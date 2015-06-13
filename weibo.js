556da6674676e6d9cb4eb54f790960f9f7745376
dsgygb <dsgygb@126.com>
2015-06-13 15:26:35 +0800

    fro weibo

---
+++ weibo.js
@@ -0,0 +1,45 @@
+/**
+ 微博粉丝服务平台
+ */
+var express = require('express');
+var weibo={};
+var router = express.Router();
+var app = express();
+var config=require('./config.js');
+/**
+ * 检查签名
+ */
+var checkSignature = function (query, token) {
+
+    if(!query.signature || !query.timestamp ||!query.nonce ||!token){
+        return false;
+    }
+
+    var signature = query.signature;
+    var timestamp = query.timestamp;
+    var nonce = query.nonce;
+
+    var shasum = crypto.createHash('sha1');
+    var arr = [token, timestamp, nonce].sort();
+    shasum.update(arr.join(''));
+
+    return shasum.digest('hex') === signature;
+};
+
+
+router.get('/weibo',function(req,res,next){
+
+    if(checkSignature(req.query,config.weibo.appkey)){
+        res.end(req.body.echostr);
+    }else{
+        res.end('0');
+    };
+
+});
+
+app.use('/',router);
+
+
+app.listen(8121,function(){
+    console.log('8121');
+});

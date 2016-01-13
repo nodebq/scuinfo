6520e1d976fb5cb6efb8d4fd100a6c8c39284cd1
dsgygb <dsgygb@126.com>
2016-01-13 16:22:21 +0800

    for update

--- test/t.js
+++ test/t.js
@@ -1,49 +1,82 @@
 var request = require('request');
 var fs = require('fs');
 var FormData = require('form-data');
+var conn = require('../libs/mysql.js')
 var weiboToken = {
 
 };
 
-fs.readFile('../token/weibo_token.txt', 'utf8', function (err, txt) {
-    //console.log(err,txt);//return;
-    if (err) {
-        console.log(err+new Date());
-        console.log('微博初始化失败');
-        return;
-    }
-//console.log(txt);
-    try{
-        weiboToken=JSON.parse(txt);
-        //console.log('初始化微博分享');
-    }catch(e){
-        weiboToken={}
-    }
+conn.query(
+    {
+        sql:"select * from secret_weibo_query where status=0 limit 0,1"
+    },function(e,r) {
+        if (e) {
+            console.log(e + new Date());
+            return;
+        }
+        //console.log(r);
+        if (r.length > 0) {
 
-});
+            conn.query(
 
-var form = new FormData();
+                {
+                    sql:"select * from secret_post where id="+r[0].postId
+                },function(ee,rr){
+                    console.log(rr[0].content);
 
-form.append('status', 'test2');
-form.append('visible',1);
-form.append('access_token','2.00juRUzDLyE8QB019f190654bwq3eC');
-form.append('pic', request('http://static.scuinfo.com/uploads/0245f1ffa15a9a687405c880464ccf05.jpg'));
 
-form.submit('https://upload.api.weibo.com/2/statuses/upload.json', function(err, res,body) {
-    // res – response object (http.IncomingMessage)  //
-    res.resume();
+                    var x = rr[0].content.split("\n");
+                    console.log(x.join("\n "));
+                }
+            )
+        }
 
-    var body = '';
-    res.on('data', function(chunk) {
-        //console.log(chunk);
-        body += chunk;
-    });
-    res.on('end', function() {
-        console.log(body);
+
     });
 
 
-});
+
+
+
+//fs.readFile('../token/weibo_token.txt', 'utf8', function (err, txt) {
+//    //console.log(err,txt);//return;
+//    if (err) {
+//        console.log(err+new Date());
+//        console.log('微博初始化失败');
+//        return;
+//    }
+////console.log(txt);
+//    try{
+//        weiboToken=JSON.parse(txt);
+//        //console.log('初始化微博分享');
+//    }catch(e){
+//        weiboToken={}
+//    }
+//
+//});
+//
+//var form = new FormData();
+//
+//form.append('status', 'test2');
+//form.append('visible',1);
+//form.append('access_token','2.00juRUzDLyE8QB019f190654bwq3eC');
+//form.append('pic', request('http://static.scuinfo.com/uploads/0245f1ffa15a9a687405c880464ccf05.jpg'));
+//
+//form.submit('https://upload.api.weibo.com/2/statuses/upload.json', function(err, res,body) {
+//    // res – response object (http.IncomingMessage)  //
+//    res.resume();
+//
+//    var body = '';
+//    res.on('data', function(chunk) {
+//        //console.log(chunk);
+//        body += chunk;
+//    });
+//    res.on('end', function() {
+//        console.log(body);
+//    });
+//
+//
+//});
 
 
 
